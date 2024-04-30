6fdfbf11b9d86bfbabbfd45f0ff295ffc3302d15
razorwarecc <108592787+razorwarecc@users.noreply.github.com>
2024-04-30 19:09:46 +0000

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,6 @@
+if not isfolder("fentanyl.club/tracerassets") then makefolder("fentanyl.club/tracerassets") end
+if not isfile("fentanyl.club/tracerassets/arrow1.png") then writefile("fentanyl.club/tracerassets/arrow1.png", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/tracerimages/main/arrow1.png")) end
+if not isfile("fentanyl.club/tracerassets/arrow2.png") then writefile("fentanyl.club/tracerassets/arrow2.png", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/tracerimages/main/arrow2.png")) end
+if not isfile("fentanyl.club/tracerassets/hanime.png") then writefile("fentanyl.club/tracerassets/hanime.png", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/tracerimages/main/hanime.png")) end
+if not isfile("fentanyl.club/tracerassets/hexagon.png") then writefile("fentanyl.club/tracerassets/hexagon.png", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/tracerimages/main/hexagon.png")) end
+if not isfile("fentanyl.club/tracerassets/troll.png") then writefile("fentanyl.club/tracerassets/troll.png", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/tracerimages/main/troll.png")) end
